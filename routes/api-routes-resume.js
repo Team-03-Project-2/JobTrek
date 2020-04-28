eaac429ba8e73e6926ca96273b6d556bb9f2a8e7
CrisTerry <crisdc88@gmail.com>
2020-04-28 14:36:40 -0700

    add modal

--- routes/api-routes-resume.js
+++ routes/api-routes-resume.js
@@ -5,12 +5,24 @@ module.exports = function (app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.get("/api/resume", function (req, res) {
+  app.get("/api/resume/all", function (req, res) {
     db.resume.findAll().then(function(data){
     res.json(data);
    });
     
   });
+
+  app.post("/api/resume/create", function(req, res){
+    let id= req.body.userId;
+    let star = false;
+    let role = req.body.role;
+    let fileName = req.body.file;
+
+    // create folder if it doesn't exists docs  
+    // fs save file location
+    
+
+  })
 
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
