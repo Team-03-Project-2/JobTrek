13acefa74263ff75c9cb75661fd6bea28ed3bc2e
Juliet George <georgejuliet11@gmail.com>
2020-05-01 13:59:52 -0700

    update

--- public/js/jobboard.js
+++ public/js/jobboard.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
 
 $(".createJobSubmit").on("click", function (event) {
         //    event.preventDefault();
-        
+        console.log("add button listens")
         var jobData = {
          job_title:$("#title").val(),
          describe:$("#jobdescription").val(),
@@ -35,23 +35,64 @@ $(".createJobSubmit").on("click", function (event) {
                 type: "POST",
                 data: jobData
             }).then(function () {
-                location.reload();
+                //location.reload();
             });
     });
 
 })
 
-$(".deleteJob").on("click", function (event) {
-    var id = $(this).data("delete");
 
+
+$(".updatejobbtn").on("click", function (event) {
+  // Make sure to preventDefault on a submit event.
+  event.preventDefault();
+console.log("update button working")
+  var updatedData = {
+      job_title: $("#title").val(),
+      describe: $("#jobdescription").val(),
+      require: $("#jobrequirement").val(),
+      locate: $("#location").val(),
+      //status:$("").val()
+      note: $("#jobNotes").val(),
+      jobUrl: $("#jobUrl").val()
+  };
+
+  var id = $(this).data("id");
+
+  // Send the POST request.
+  $.ajax("/api/jobboard/" + id, {
+      type: "PUT",
+      data: updatedData
+  }).then(
+      function () {
+          console.log("updated job");
+          // Reload the page to get the updated list
+          location.assign("/members/jobboard");
+      }
+  );
+});
+
+//delete button is working but nothing seems to be getting deleted.
+
+$("#deleteJob").on('click', function (event) {
+
+ 
+    var id = $(this).data("delete");
+    // var userid = $(this).data("userid")
+
+    console.log(id)
     // This is the client's way of sending the DELETE request to the server.
-    $.ajax("/api/jobboard/" + id, {
-      type: "DELETE"
+    $.ajax("/api/jobboard", {
+      type: "DELETE",
+      data:{
+        user_id: userid,
+          id: id
+      }
     }).then(
       function () {
-        console.log("deleted id ", id);
-        // Reload the page to get the updated list
-        location.reload();
+        console.log("deleted id");
+        // Reload the page to get the updated column
+        //location.reload();
       }
     );
   });
--- routes/jobboard-routes.js
+++ routes/jobboard-routes.js
@@ -58,141 +58,150 @@ module.exports = function (app) {
             user_id: req.user_id,
             where: { status: "wishlist" }
         }).then(function (wishlistData) {
-            console.log(wishlistData)
+            //console.log(wishlistData)
             db.Job.findAll({
                 user_id: req.user_id,
-                where: { status: "wishlist" }
-            }).then(function(appliedData){
-                res.render("jobboard", {wishlist: wishlistData,
-                applied:appliedData})
+                where: { status: "applied" }
+            }).then(function (appliedData) {
+                db.Job.findAll({
+                    user_id: req.user_id,
+                    where: { status: "response" }
+                }).then(function (responseData) {
+                    res.render("jobboard", {
+                        wishlist: wishlistData,
+                        applied: appliedData,
+                        response: responseData
+                    })
+                })
+
             })
-            
-        })
 
 
-    });
-
-    app.get("/members/jobinfo", isAuthenticated, function (req, res) {
-
-        res.render("jobinfo")
-    });
-
-    app.get("/members/findjobs", isAuthenticated, function (req, res) {
-
-        res.sendFile(path.join(__dirname, '../public', 'jobapi.html'));
-        //res.json("something good is coming")
-
-    });
-
-    // app.get("/api/jobboard", function (req, res) {
-
-
-    //     db.Job.findAll({
-
-    //         user_id: req.user.id
-    //     }).then(function (dbJobboard) {
-
-    //         res.json(dbJobboard);
-    //     });
-    // });
-
-    app.get("/api/jobboard/company", function (req, res) {
-        db.Company.findAll({ user_id: 1 }
-
-        ).then(function (data) {
-
-
-            res.render("addjob", { companies: data })
-
-        })
+        });
     })
 
-    app.get("/api/jobboard/resume", function (req, res) {
-        db.Resume.findAll({ user_id: 1 }).then(function (data) {
-            //res.json(data);
-            res.render("addjob", { resumes: data })
-            //console.log(data)
+        app.get("/members/jobinfo", function (req, res) {
+
+            res.render("jobinfo")
+        });
+
+        app.get("/members/findjobs", isAuthenticated, function (req, res) {
+
+            res.sendFile(path.join(__dirname, '../public', 'jobapi.html'));
+            //res.json("something good is coming")
+
+        });
+
+        // app.get("/api/jobboard", function (req, res) {
+
+
+        //     db.Job.findAll({
+
+        //         user_id: req.user.id
+        //     }).then(function (dbJobboard) {
+
+        //         res.json(dbJobboard);
+        //     });
+        // });
+
+        app.get("/api/jobboard/company", function (req, res) {
+            db.Company.findAll({ user_id: 1 }
+
+            ).then(function (data) {
+
+
+                res.render("addjob", { companies: data })
+
+            })
         })
-    })
 
-
-    //create
-    app.post("/api/jobboard", isAuthenticated, function (req, res) {
-        //console.log("post method")
-
-        //user_id: req.user.id,
-        var jobObject = {
-            user_id: req.user_id,
-            job_title: req.body.job_title,
-            description: req.body.describe,
-            requirement: req.body.require,
-            location: req.body.locate,
-            //status:req.body.status
-            //company:req.body.company, querycompany table
-            notes: req.body.note,
-
-            url: req.body.jobUrl
-        }
-        //console.log(jobObject)
-        db.Job.create(jobObject).then(function (jobDB) {
-            res.json(jobDB)
+        app.get("/api/jobboard/resume", function (req, res) {
+            db.Resume.findAll({ user_id: 1 }).then(function (data) {
+                //res.json(data);
+                res.render("addjob", { resumes: data })
+                //console.log(data)
+            })
         })
 
 
-    });
+        //create
+        app.post("/api/jobboard", function (req, res) {
+            console.log("post method")
 
-    //update
-    app.put("api/jobboard", function (req, res) {
-
-        db.Job.findOne({
-            where: {
-                id: req.body.id
-            }
-        }).then((updateJob) => {
-            updateJob.update({
+            //user_id: req.user.id,
+            var jobObject = {
+                user_id: req.user_id,
                 job_title: req.body.job_title,
-                description: req.body.description,
-                requirement: req.body.requirement,
-                location: req.body.location,
-                // company://company_id:ref
-                // contact://contact_id:ref
-                // resume:// resume_id:ref
-                // status://status:ref
-                notes: req.body.notes,
-                url: req.body.url
-            }).then(updatedJob => {
-                res.json(updatedJob)
+                description: req.body.describe,
+                requirement: req.body.require,
+                location: req.body.locate,
+                //status:req.body.status
+                //company:req.body.company, querycompany table
+                notes: req.body.note,
+
+                url: req.body.jobUrl
+            }
+            //console.log(jobObject)
+            db.Job.create(jobObject).then(function (jobDB) {
+                res.json(jobDB)
             })
+
+
+        });
+
+        //update
+        app.put("/api/jobboard", isAuthenticated, function (req, res) {
+
+            db.Job.findOne({
+                where: {
+                    id: req.body.id
+                }
+            }).then((updateJob) => {
+                updateJob.update({
+                    job_title: req.body.job_title,
+                    description: req.body.description,
+                    requirement: req.body.requirement,
+                    location: req.body.location,
+                    // company://company_id:ref
+                    // contact://contact_id:ref
+                    // resume:// resume_id:ref
+                    // status://status:ref
+                    notes: req.body.notes,
+                    url: req.body.url
+                }).then(updatedJob => {
+                    res.json(updatedJob)
+                })
+                    .catch(err => {
+                        throw err
+                    })
+            })
+        });
+
+
+        app.delete("/api/jobboard", function (req, res) {
+
+            db.Job.destroy({
+
+                where: {
+
+                    id: req.body.id,
+                    user_id: req.body.user_id
+                }
+            })
+                .then(deletedJob => {
+                    res.json(deletedJob)
+                })
                 .catch(err => {
+                    // throw err;
                     throw err
                 })
-        })
-    });
-
-
-    app.delete("api/jobboard", function (req, res) {
-
-        db.Job.destroy({
-
-            where: {
-
-                id: req.body.id
-            }
-        })
-            .then(deletedJob => {
-                res.json(deletedJob)
-            })
-            .catch(err => {
-                // throw err;
-                throw err
-            })
-    });
+        });
 
 
 
 
 
-}
+    }
 
 
 
