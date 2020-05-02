cb3814a14a2daadb119b3494109d21213592cc34
Kasey Chang <kschang77@yahoo.com>
2020-05-01 20:02:27 -0700

    20200501 KC tasks CRUD complete, UI tweaks

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var session = require("express-session");
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
 
+var moment = require('moment');
+moment().format();
+
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
 var db = require("./models");
@@ -18,8 +21,12 @@ app.use(express.static("./public"));
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({
+  defaultLayout: "main",
+  helpers: require('./config/handlebars-helpers.js').helpers
+}));
 app.set("view engine", "handlebars");
+
 
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
@@ -30,6 +37,8 @@ app.use(passport.session());
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 require("./routes/api-routes-resume.js")(app);
+require("./routes/api-routes-task.js")(app);
+
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function () {
